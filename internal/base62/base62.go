e74227d3eb338b31e14fae58449a2c6016282227
Zapi-web <screbov2@gmail.com>
2026-08-08 15:30:05 +0200

    feat: implement snowflake id generator and add base62 encoding

---
+++ internal/base62/base62.go
@@ -0,0 +1,69 @@
+package base62
+
+import (
+	"errors"
+	"math"
+
+	"github.com/Zapi-web/url-shortener/internal/domain"
+)
+
+type Base62Encoder struct{}
+
+const base62Alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var decodeMap [256]int8
+
+func init() {
+	for i := 0; i < len(decodeMap); i++ {
+		decodeMap[i] = -1
+	}
+
+	for i := 0; i < len(base62Alphabet); i++ {
+		decodeMap[base62Alphabet[i]] = int8(i)
+	}
+}
+
+func New() *Base62Encoder {
+	return &Base62Encoder{}
+}
+
+func (e *Base62Encoder) Encode(num uint64) string {
+	if num == 0 {
+		return "0"
+	}
+
+	var buf [11]byte
+	i := len(buf)
+
+	for num > 0 {
+		i--
+		buf[i] = base62Alphabet[num%62]
+		num /= 62
+	}
+
+	return string(buf[i:])
+}
+
+func (e *Base62Encoder) Decode(str string) (uint64, error) {
+	if str == "" {
+		return 0, domain.ErrInputisEmpty
+	}
+
+	var res uint64
+
+	for i := 0; i < len(str); i++ {
+		val := decodeMap[str[i]]
+
+		if val == -1 {
+			return 0, errors.New("invalid base62 character")
+		}
+
+		if res > (math.MaxUint64-uint64(val))/62 {
+			return 0, errors.New("uint64 is overflowed")
+		}
+
+		res = res*62 + uint64(val)
+	}
+
+	return res, nil
+}
